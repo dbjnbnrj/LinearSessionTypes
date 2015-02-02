6cfa175b92e91f06b7a9605d9b45bfd75c58066d
Debjani Banerjee <dbjnbnrj@gmail.com>
2015-02-02 12:36:47 -0800

    Cleaning files

--- pgm.py
+++
@@ -1,7 +0,0 @@
-from types import * 
-
-c1 = Channel("hello world")
-c2 = Channel("what?") 
-recv = TyRecv(TyBool(), TyEnd())
-qual = TyQual(Linear(), TyRecv(), Inaction()) 
-ScopeRestriction( c1, c2, qual, Inaction() )
